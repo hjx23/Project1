53895bb16f326c2c4985a64184b5be810a3b61d5
JinX <seeyoulx23@gmail.com>
2026-08-09 10:25:33 +0800

    优化异常处理：打印所有异常

--- read_txt.py
+++ read_txt.py
@@ -2,17 +2,22 @@ from pathlib import Path
 
 def read_single_txt(file_path: str | Path):
     path = Path(file_path)
-    # 判断文件是否存在
+    #判断文件是否存在
     if not path.exists():
-        print("目标文件不存在")
+        print("文件不存在！！！")
         return ""
-    # 双编码兜底读取
+    #双保险：如果utf-8格式不对的话，那么可以使用gbk格式去解析
     try:
-        with open(path, "r", encoding="utf-8") as f:
+        with open(path,"r",encoding="utf-8") as f:
             return f.read()
     except UnicodeDecodeError:
-        with open(path, "r", encoding="gbk") as f:
+        with open(path,"r",encoding="gbk") as f:
             return f.read()
+    #上述代码存在问题：如果我传入的文件路径是文件夹的路径，程序不会报错也不会有结果
+    #所以我需要继续进行报错处理
+    except Exception as e:
+        print(f"读取失败：{e}")
+        return ""
 
 if __name__ == "__main__":
     content = read_single_txt("./docs/test.txt")
